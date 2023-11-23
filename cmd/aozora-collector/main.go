b0415a5484f7c0763b3f125497f865a80261c07f
light-planck <seseifunc16@gmail.com>
2023-11-24 01:05:12 +0900

    .

--- cmd/aozora-collector/main.go
+++ cmd/aozora-collector/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"archive/zip"
 	"bytes"
+	"database/sql"
 	"errors"
 	"fmt"
 	"io"
@@ -14,6 +15,9 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/ikawaha/kagome-dict/ipa"
+	"github.com/ikawaha/kagome/v2/tokenizer"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Entry struct {
@@ -26,6 +30,12 @@ type Entry struct {
 }
 
 func main() {
+	db, err := setupDB("database.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
 
 	entries, err := findEntries(listURL)
@@ -40,9 +50,33 @@ func main() {
 			continue
 		}
 
-		fmt.Println(entry.SiteURL)
-		fmt.Println(content)
+		err = addEntry(db, &entry, content)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
 	}
+}
+
+func setupDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	queries := []string{
+		`CREATE TABLE IF  NOT EXISTS authors(author_id TEXT, author TEXT, PRIMARY KEY(author_id))`,
+		`CREATE TABLE IF NOT EXISTS contents(author_id TEXT, title_id TEXT, title TEXT, content TEXT, PRIMARY KEY(author_id, title_id))`,
+		`CREATE VIRTUAL TABLE IF NOT EXISTS contents_fts USING fts4(words)`,
+	}
+	for _, query := range queries {
+		_, err = db.Exec(query)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return db, nil
 }
 
 func findEntries(siteURL string) ([]Entry, error) {
@@ -163,4 +197,46 @@ func extractText(zipURL string) (string, error) {
 	}
 
 	return "", errors.New("contents not found")
+}
+
+func addEntry(db *sql.DB, entry *Entry, content string) error {
+	_, err := db.Exec(`
+		REPLACE INTO authors(author_id, author) VALUES(?, ?)
+	`,
+		entry.AuthorID,
+		entry.Author,
+	)
+	if err != nil {
+		return err
+	}
+
+	res, err := db.Exec(`
+		REPLACE INTO  contents(author_id, title_id, title, content) values(?, ?, ?, ?)
+	`,
+		entry.AuthorID,
+		entry.TitleID,
+		entry.Title,
+		content,
+	)
+	if err != nil {
+		return err
+	}
+
+	docID, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	t, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	seg := t.Wakati(content)
+	_, err = db.Exec(`REPLACE INTO contents_fts(docid, words) values(?, ?)`, docID, strings.Join(seg, " "))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.21.4
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/ikawaha/kagome-dict v1.0.9 // indirect
+	github.com/ikawaha/kagome-dict/ipa v1.0.10 // indirect
+	github.com/ikawaha/kagome/v2 v2.9.4 // indirect
 	github.com/mattn/go-sqlite3 v1.14.18 // indirect
 	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
