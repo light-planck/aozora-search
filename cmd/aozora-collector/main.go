6f2efb3c8b27464b89dd8ad7ee59107d12c9aefb
light-planck <seseifunc16@gmail.com>
2023-11-23 14:14:36 +0900

    feat: エントリーとしてまとめる

--- cmd/aozora-collector/main.go
+++ cmd/aozora-collector/main.go
@@ -17,7 +17,7 @@ type Entry struct {
 	Author   string
 	TitleID  string
 	Title    string
-	InfoURL  string
+	SiteURL  string
 	ZipURL   string
 }
 
@@ -37,18 +37,31 @@ func findEntries(siteURL string) ([]Entry, error) {
 	}
 
 	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html`)
+	entries := []Entry{}
 	doc.Find("ol li a").Each(func(_ int, elem *goquery.Selection) {
 		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
 		if len(token) != 3 {
 			return
 		}
 
+		title := elem.Text()
 		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
-		_, zipURL := findAuthorAndZIP(pageURL)
+		author, zipURL := findAuthorAndZIP(pageURL)
+
+		if zipURL != "" {
+			entries = append(entries, Entry{
+				AuthorID: token[1],
+				Author:   author,
+				TitleID:  token[2],
+				Title:    title,
+				SiteURL:  siteURL,
+				ZipURL:   zipURL,
+			})
+		}
 		println(zipURL)
 	})
 
-	return nil, nil
+	return entries, nil
 }
 
 func findAuthorAndZIP(siteURL string) (string, string) {
